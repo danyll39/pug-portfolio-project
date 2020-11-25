11f2a1e6be672dea4322cda24832a3cea13c7651
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-11-24 23:51:29 -0500

    Add data and install more dependencies

---
+++ data.js
@@ -0,0 +1,59 @@
+const data = {
+  title: 'Super Mario World',
+  gameSystem: ' Super Nintendo Entertainment System ',
+  releaseDate: 'November 21, 1990',
+  createdBy: 'Shigeru Miyamoto',
+  characters: [
+    {
+      name: 'Mario',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      occupation: 'plumber',
+      origin: 'Brooklyn, NY',
+      // eslint-disable-next-line max-len
+      biography: 'Since Mario and his brother Luigi were sucked into a warp-drain and ended up in the Mushroom World they have befriended Princess Toadstool, the ruler of the Mushroom Kingdom and helped in her plight against the evil King Koopa and his seven menacing Koopa Kids ever since. Mario is well respected by the people of the Mushroom Kingdom for his skills in battle and resilience against the Koopa clan',
+    },
+    {
+      name: 'Luigi',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      occupation: 'plumber',
+      origin: 'Brooklyn, NY',
+      // eslint-disable-next-line max-len
+      biography: 'Marios taller little brother. Although the two are known to argue (like all brothers) when they are working together against a common enemy they are a formidable force. There is perhaps a slight element of jealousy from Luigi toward Mario as he always gets less recognition for this work than his older brother',
+    },
+    {
+      name: 'Peach',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Female',
+      occupation: 'Ruler of Mushroom Kingdom',
+      origin: 'Mushroom Kingdom',
+      // eslint-disable-next-line max-len
+      biography: 'Peach Mario\'s love interest and the frequent damsel in distress. She is also good friends with Luigi, Toad, and especially Princess Daisy. Super Mario Bros was Princess Peach\'s debut appearance. Bowser had invaded the Mushroom Kingdom, and transformed its inhabitants into blocks and other objects.',
+    },
+    {
+      name: 'Yoshi',
+      status: 'Alive',
+      species: 'Yoshi',
+      gender: 'Male',
+      occupation: '',
+      origin: 'Yoshi\'s island',
+      // eslint-disable-next-line max-len
+      biography: 'Yoshi is a green dinosaur who appears frequently through out as a main character in the Super Mario series',
+    },
+    {
+      name: 'Bowser',
+      status: 'Alive',
+      species: 'Koopa',
+      gender: 'Male',
+      occupation: 'Ruler of the Koopa Kingdom',
+      origin: 'Koopa Kingdom',
+      biography: 'Bowser is the leader of the turtle-like Koopa race and has been the archenemy of Mario since his first appearance, in the 1985 video game Super Mario Bros. His ultimate goals are to kidnap Princess Peach, defeat Mario and conquer the Mushroom Kingdom',
+    }
+  ],
+}
+
+module.exports = data
