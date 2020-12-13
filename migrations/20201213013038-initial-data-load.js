4450274d762b6e79160d9e70f642da6134e725d8
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-12 20:33:32 -0500

    Add load data file and load data into file

---
+++ migrations/20201213013038-initial-data-load.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert('drinks', [
+      {
+        name: 'Cape Cod', alcohol: 'Vodka', mixer: 'cranberry', abv: 14, garnish: 'lime'
+      },
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    ])
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.bulkDelete('drinks')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
