a30c9b6d62b504df9ef1e2284b71944cdc5340c4
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-22 18:16:24 -0500

    Update index to add new controllers

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require('express')
-const data = require('./data')
+const bodyParser = require('body-parser')
+const { getAllDrinks, saveNewDrink, deleteDrink, getDrinkByName } = require('./controllers/drinks')
+// eslint-disable-next-line max-len
+const { getAllDrinksByAlcohol, getDrinksByAlcoholName, saveNewAlcohol, deleteAlcohol } = require('./controllers/alcohol')
+const drinks = require('./models')
 const app = express()
-
-
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-  return response.render('index', { data })
+  return response.render('index', { drinks })
 })
-app.get('/drinks', (request, response) => {
-  return response.render('drinks')
-})
+
+// app.get('/drinks', getAllDrinks )
+
+app.get('/drinks', getAllDrinks)
+app.get('/drinks/:identifier', getDrinkByName)
+app.post('/drinks', bodyParser.json(), saveNewDrink)
+app.delete('/drinks/:name', bodyParser.json(), deleteDrink)
+
+
+app.get('/alcohol', getAllDrinksByAlcohol)
+app.get('/alcohol/:identifier', getDrinksByAlcoholName)
+app.post('/alcohol', bodyParser.json(), saveNewAlcohol)
+app.delete('/alcohol/:name', bodyParser.json(), deleteAlcohol)
+
+
+
+
+// app.get('/drinks/random', (request, response) => {
+//   getRandomDrink(request, response)
+// })
+// app.get('/drinks/:identifier', getDrinksByAlcohol)
 // app.get('/:drinks', (request, response) => {
 //   const alcohol = data.alcohol === request.params.id
 
