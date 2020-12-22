be53dd153e736a7e8b722fb4d67d3cd69a38760d
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-22 01:49:40 -0500

    Update index model to include new model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize')
 const allConfigs = require('../config/sequelize')
 const DrinksModel = require('./drinks')
+const DirectionsModel = require('./directions')
+const GlasswareModel = require('./glassware')
 
 const environment = process.env.NODE_ENV || 'development'
 const config = allConfigs[environment]
@@ -8,6 +10,13 @@ const config = allConfigs[environment]
 const connection = new Sequelize(config.database, config.username, config.password, {
   host: config.host, dialect: config.dialect
 })
-const Drinks = DrinksModel(connection, Sequelize)
 
-module.exports = { Drinks }
+const Drinks = DrinksModel(connection, Sequelize)
+const Directions = DirectionsModel(connection, Sequelize, Drinks)
+// const Glassware = GlasswareModel(connection, Sequelize, Directions)
+
+Drinks.belongsTo(Directions)
+Directions.hasMany(Drinks)
+
+
+module.exports = { Drinks, Directions, Op: Sequelize.Op, }
