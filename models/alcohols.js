3175b1f3aff2075f3ae65bf3e42e0960ee49ffd2
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-01-23 21:21:05 -0500

    Update models

--- models/alcohols.js
+++ models/alcohols.js
@@ -1,14 +1,13 @@
-const Alcohols = (connection, Sequelize) => {
-  return connection.define('alcohols', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: Sequelize.STRING },
-    abv: { type: Sequelize.INTEGER },
-  }, {
-    defaultScope: {
-      attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] }
-    }
-  }, { paranoid: true })
-}
+export default (connection, Sequelize) => connection.define('alcohols', {
+  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+  name: { type: Sequelize.STRING },
+  abv: { type: Sequelize.INTEGER },
+}, {
+  defaultScope: {
+    attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] }
+  }
+}, { paranoid: true })
 
 
-module.exports = Alcohols
+
+
