e9fdb3a61b6045e9d826b9597489a21ae655c80f
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-01-23 21:19:44 -0500

    Update server routes and add new route

--- server.js
+++ server.js
@@ -3,24 +3,31 @@ import bodyParser from 'body-parser'
 import path from 'path'
 import { getAllDrinks, saveNewDrink, deleteDrink, getDrinkByName } from './controllers/drinks'
 // eslint-disable-next-line max-len
-import { getAllDrinksByAlcohol, getDrinksByAlcoholName, saveNewAlcohol, deleteAlcohol } from './controllers/alcohol'
+import { getAllDrinksByAlcohol, getDrinksByAlcoholName, getAlcoholById, saveNewAlcohol, deleteAlcohol } from './controllers/alcohols'
+
 const app = express()
 
-app.set('view engine', 'pug')
+app.set('view engine','pug')
 app.use(express.static('public'))
 
-app.get('/', (request, response) => response.render('index'))
-app.use(express.static('public'))
+
+app.get('/api', (request, response) => response.status(200).render('index'))
+
+app.get('/', (request, response) => response.redirect('/api/simple'))
+// app.use(express.static('public'))
 
 app.get('/api/drinks', getAllDrinks)
-app.get('/api/drinks/:identifier', getDrinkByName)
+app.get('/api/drinks/:id', getDrinkByName)
 app.post('/api/drinks', bodyParser.json(), saveNewDrink)
 app.delete('/api/drinks/:name', bodyParser.json(), deleteDrink)
 
-app.get('/api/alcohol', getAllDrinksByAlcohol)
-app.get('/api/alcohol/:identifier', getDrinksByAlcoholName)
-app.post('/api/alcohol', bodyParser.json(), saveNewAlcohol)
-app.delete('/api/alcohol/:name', bodyParser.json(), deleteAlcohol)
+app.get('/api/alcohols', getAllDrinksByAlcohol)
+app.get('/api/alcohols/name/:name', getDrinksByAlcoholName)
+app.get('/api/alcohols/:id', getAlcoholById)
+app.post('/api/alcohols', bodyParser.json(), saveNewAlcohol)
+app.delete('api/alcohols/:name', bodyParser.json(), deleteAlcohol)
+
+
 
 app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
 app.listen(1144, () => {
